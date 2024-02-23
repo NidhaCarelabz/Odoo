3aba21284ff4bcc9de8188cf405fa4e2a6c435da
Silpa-sudhakaran <carelabsit01@gmail.com>
2024-02-23 14:17:17 +0530

    custom fields

---
+++ carelabs/models/leadinherit.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class CrmLeadInherit(models.Model):
+    _inherit = 'crm.lead'
+
+    x_contact_medium = fields.Char(string="Contact Medium")
+
+    service = fields.Many2many(
+        'product.product', string='Services',
+        help='Select services related to this lead.'
+    
+    )
